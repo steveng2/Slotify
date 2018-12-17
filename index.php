b8eabc8516c5649911fa98e10bff13d63193a530
steven garner <stevengarner3@gmail.com>
2018-12-17 03:05:26 +0000

    add albumArtwork to side.  Added progress bar.

--- index.php
+++ index.php
@@ -20,11 +20,17 @@ else {
 </head>
 <body>
  
-  <div id="nowPlayingBarContainer">
+     <div id="nowPlayingBarContainer">
       <div id="nowPlayingBar">
           
           <div id="nowPlayingLeft">
-              
+              <div class="content">
+                  <span class="albumLink">
+                     
+                      <img src="assets/images/squareart.png" alt="AlbumArtwork" class="albumArtwork">
+                  </span>
+                  
+              </div>
           </div>
       
           <div id="nowPlayingCenter">
@@ -37,28 +43,39 @@ else {
                               <img src="assets/images/icons/shuffle.png" alt="Shuffle">
                           </button>
                           
-                          <button class="controlButton shuffle" title="Shuffle button">
+                          <button class="controlButton previous" title="Previous button">
                               <img src="assets/images/icons/previous.png" alt="Previous">
                           </button>
                           
-                          <button class="controlButton shuffle" title="Play button">
+                          <button class="controlButton play" title="Play button">
                               <img src="assets/images/icons/play.png" alt="Play button">
                           </button>
                           
-                           <button class="controlButton shuffle" title="Pause button" style="display: none;">
+                           <button class="controlButton pause" title="Pause button" style="display: none;">
                               <img src="assets/images/icons/pause.png" alt="Pause button">
                           </button>
                           
-                          <button class="controlButton shuffle" title="repeat button">
+                          <button class="controlButton repeat" title="repeat button">
                               <img src="assets/images/icons/repeat.png" alt="Repeat">
                           </button>
                            
-                          <button class="controlButton shuffle" title="Next button">
+                          <button class="controlButton next" title="Next button">
                               <img src="assets/images/icons/next.png" alt="next">
                           </button>
                   
                       </div>
-              
+                      
+                      <div class="playbackBar">
+                          <span class="progressTime current">0.00</span>
+                          <div class="progressBar">
+                              <div class="progressBarBg">
+                                  <div class="progress"></div>  
+                              </div>
+                          </div>
+                          <span class="progressTime remaining">0.00</span>
+                          
+                      </div>
+                  
               </div>
               
           </div>
@@ -69,8 +86,6 @@ else {
           
       </div>
       
-  </div>
-    
-    
+  </div> 
 </body>
 </html>
